da11f7d6a350a1e9c057b463747c85009f8555b9
Lucas-Kohorst <kohorstlucas@gmail.com>
2023-06-24 16:32:17 -0400

    update

--- app/api/keys/pgp/route.ts
+++ app/api/keys/pgp/route.ts
@@ -3,66 +3,100 @@ import { cookies } from 'next/headers';
 import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
 import { getURL } from '@/utils/helpers';
 import { Database } from '@/types_db';
+import { NextResponse } from 'next/server'
 
-export async function GET(req: Request) {
-    if (req.method === 'GET') {
-        try {
-            const supabase = createRouteHandlerClient<Database>({ cookies });
-            const {
-                data: { user }
-            } = await supabase.auth.getUser();
+export async function GET() {
+    const data = await {
+        name: "name",
+        email: "email",
+    }
 
-            if (user == null) {
-                return new Response(
-                    JSON.stringify({ error: { statusCode: 401, message: "Unauthorized, Sign In" } }),
-                    {
-                        status: 401
-                    }
-                );
-            } else {
-                console.log(user)
-                // const name = user?.name
-                const name = "name"
-                const email = user?.email
+    try {
+        const supabase = createRouteHandlerClient<Database>({ cookies });
+        const {
+            data: { user }
+        } = await supabase.auth.getUser();
 
-                const passphrase = "passphrase"
+        console.log(user)
 
-                if (
-                    typeof name !== "string" ||
-                    typeof email !== "string" ||
-                    typeof passphrase !== "string"
-                ) {
-                    return new Response(
-                        JSON.stringify({ error: { statusCode: 401, message: "Unauthorized, Sign In" } }),
-                        {
-                            status: 401
-                        }
-                    );
-                } else {
-                    await generateKeyPair(name, email, passphrase).then((value) => {
-                        const publicKey = value.publicKey
-                        const privateKey = value.privateKey
-                        const response = { publicKey: publicKey, privateKey: privateKey }
-                        return new Response(JSON.stringify({ response }), {
-                            status: 200
-                        });
-                    }).catch((err) => {
-                        console.log(err)
-                        return new Response(
-                            JSON.stringify({ error: { statusCode: 500, message: "Unable to generate a PGP Key" } }),
-                            {
-                                status: 500
-                            }
-                        );
-                    });
-                }
-            }
-        } catch(err) {
-            console.log(err);
-            return new Response(JSON.stringify(err), { status: 500 });
-        };
+        // get full name 
+        const { data, error } = await supabase
+            .from('users')
+            .select('full_name')
+            .eq('id', user?.id);
+
+        if (error) {
+            console.log(error);
+            return NextResponse.json({ error: 'Not Authenticated' });
+        } else {
+            return NextResponse.json({ user })
+        }
+    } catch (err) {
+        console.log(err);
+        return NextResponse.json({ error: 'Not Authenticated' });
     }
 }
+
+// export async function POST(req: Request) {
+//     console.log("GET")
+//     if (req.method === 'GET') {
+//         try {
+//             const supabase = createRouteHandlerClient<Database>({ cookies });
+//             const {
+//                 data: { user }
+//             } = await supabase.auth.getUser();
+
+//             if (user == null) {
+//                 return new Response(
+//                     JSON.stringify({ error: { statusCode: 401, message: "Unauthorized, Sign In" } }),
+//                     {
+//                         status: 401
+//                     }
+//                 );
+//             } else {
+//                 console.log(user)
+//                 // const name = user?.name
+//                 const name = "name"
+//                 const email = user?.email
+
+//                 const passphrase = "passphrase"
+
+//                 if (
+//                     typeof name !== "string" ||
+//                     typeof email !== "string" ||
+//                     typeof passphrase !== "string"
+//                 ) {
+//                     return new Response(
+//                         JSON.stringify({ error: { statusCode: 401, message: "Unauthorized, Sign In" } }),
+//                         {
+//                             status: 401
+//                         }
+//                     );
+//                 } else {
+//                     await generateKeyPair(name, email, passphrase).then((value) => {
+//                         const publicKey = value.publicKey
+//                         const privateKey = value.privateKey
+//                         const response = { publicKey: publicKey, privateKey: privateKey }
+//                         return new Response(JSON.stringify({ response }), {
+//                             status: 200
+//                         });
+//                     }).catch((err) => {
+//                         console.log(err)
+//                         return new Response(
+//                             JSON.stringify({ error: { statusCode: 500, message: "Unable to generate a PGP Key" } }),
+//                             {
+//                                 status: 500
+//                             }
+//                         );
+//                     });
+//                 }
+//             }
+//         } catch (err) {
+//             console.log(err);
+//             return new Response(JSON.stringify(err), { status: 400 });
+//         };
+//     }
+// }
 
 async function generateKeyPair(name: string, email: string, passphrase: string): Promise<{ publicKey: string, privateKey: string }> {
     const userIds = [{ name: name, email: email }]
--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -7,6 +7,7 @@ import type { Database } from '@/types_db'
 
 export async function GET(request: NextRequest) {
   const requestUrl = new URL(request.url)
+  console.log(requestUrl)
   const code = requestUrl.searchParams.get('code')
 
   if (code) {
